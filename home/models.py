694405b4ed46c3ddec8ef72dbaefa9ff978b2854
littlefermatlives <saurabhmishra16121999@gmail.com>
2021-09-06 16:18:49 +0530

    adding more functionality

--- home/models.py
+++ home/models.py
@@ -6,3 +6,7 @@ class Contact(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length = 10)
     desc = models.TextField()
+    def __str__(self):
+        return self.name
+
+
--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path, include 
 from home import views
 
+# Djang admin header customisation
+admin.site.site_header = "Login to Database"
+admin.site.site_title = "Welcome to saurabh's database"
+admin.site.index_title = "Welcome to this portal"
+
+
 urlpatterns = [
    path('', views.index, name='index'),
    path('about', views.about, name='about'),
